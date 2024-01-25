3c4d82a48af5e8a609ad5933fa27fdd9176c986f
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-25 23:26:55 +0200

    Update gp-link.js

--- plugins/gp-link.js
+++ plugins/gp-link.js
@@ -2,17 +2,17 @@ import { areJidsSameUser } from '@whiskeysockets/baileys'
 let handler = async (m, { conn, args }) => {
     let group = m.chat
     if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
-    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw '⚠️ Can only be used in groups'
+    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw '⚠️ يمكن استخدام هذا الأمر فقط في المجموعات'
     let groupMetadata = await conn.groupMetadata(group)
-    if (!groupMetadata) throw 'groupMetadata is undefined :\\'
-    if (!('participants' in groupMetadata)) throw 'participants is not defined :('
+    if (!groupMetadata) throw 'معلومات المجموعة غير معرفة :\\'
+    if (!('participants' in groupMetadata)) throw 'المشاركين غير معرفون :('
     let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
-    if (!me) throw '✳️ I am not in that group :('
-    if (!me.admin) throw '✳️ I am not an administrator'
+    if (!me) throw '✳️ لست في تلك المجموعة :('
+    if (!me.admin) throw '✳️ لست مسؤولًا'
     m.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group))
 }
 handler.help = ['link']
 handler.tags = ['group']
-handler.command = ['link', 'linkgroup'] 
+handler.command = ['لينك', 'linkgroup'] 
 
 export default handler
