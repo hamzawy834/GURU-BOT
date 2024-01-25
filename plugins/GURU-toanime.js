69faadc26e9e084be5a279b7d015472806477f33
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-25 17:52:56 +0200

    Update GURU-toanime.js

--- plugins/GURU-toanime.js
+++ plugins/GURU-toanime.js
@@ -2,8 +2,8 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ""
-if (!/image/g.test(mime)) throw '*Respond to a image*'
-m.reply('*This command can turn your photo into anime*')    
+if (!/image/g.test(mime)) throw '*يرجى الرد على صورة*'
+m.reply('*يمكن استخدام هذا الأمر لتحويل صورتك إلى شخصية أنمي*')    
 let data = await q.download?.()
 let image = await uploadImage(data)
 try {
@@ -18,7 +18,7 @@ try{
 let anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`
 await conn.sendFile(m.chat, anime3, 'error.jpg', null, m) 
 } catch (e) {
-throw '*Error check if the persons face is visible*'
+throw '*خطأ، تحقق مما إذا كان وجه الشخص مرئيًا*'
 }}}}
 handler.help = ["toanime"]
 handler.tags = ["AI"]
