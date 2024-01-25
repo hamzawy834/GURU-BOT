b941dcf6630aee02f62cd7a7a046afd5a0296b54
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-25 18:32:15 +0200

    Update _autoReply.js

--- plugins/_autoReply.js
+++ plugins/_autoReply.js
@@ -1,8 +1,8 @@
 export async function all(m) {
 	
   // when someone sends a group link to the bot's dm
-  if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('open this link')) && !m.isBaileys && !m.isGroup) {
-   this.sendMessage(m.chat,{text:`Hello @${m.sender.split('@')[0]}\nyou can rent the bot to join a group\n\n_For more info you can DM the owner_\n*Type* \`\`\`.owner\`\`\` *to contact the owner*`.trim()}, {quoted:m});
+  if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('افتح هذا الرابط')) && !m.isBaileys && !m.isGroup) {
+   this.sendMessage(m.chat,{text:`مرحبًا @${m.sender.split('@')[0]}\nيمكنك استئجار الروبوت للانضمام إلى مجموعة\n\n_لمزيد من المعلومات، يمكنك إرسال رسالة مباشرة إلى المالك_\n*النوع* \`\`\` .owner\`\`\` *للاتصال بالمالك*`.trim()}, {quoted:m});
    /*this.sendButton(m.chat, `*Invite bot to a group*      
     Hallo @${m.sender.split('@')[0]} 
     you can rent the bot to join a group or contact owner 
