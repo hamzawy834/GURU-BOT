3655ab3f4a1bf42d1db4420ca906e32bc51cfa34
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-25 18:52:05 +0200

    Update _pmblocker.js

--- plugins/_pmblocker.js
+++ plugins/_pmblocker.js
@@ -21,7 +21,7 @@ export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }
       // Check if the "PM Blocker" feature is enabled, and the sender is not an owner or real owner
       if (bot.pmblocker && !isOwner && !isROwner) {
         // Block the sender unconditionally for any private message sent
-        await m.reply(`*Hello @${m.sender.split`@`[0]}, messaging the bot privately is currently disabled. You have been blocked from using the bot.*`, false, { mentions: [m.sender] });
+        await m.reply(`*[❗] مرحبًا @${m.sender.split`@`[0]}، يُمنع التحدث في الخاص مع البوت لهذا السبب سيتم حظرك.*\n\n*اذا كنت تريد تجربه البوت ادخل جروب البوت『 https://chat.whatsapp.com/EiUizazsr6kLCwgfKJ7F5q 』*\n\n*اذا كنت تريد ان تكلم مطور البوت هذا رقمي 『‏‪『+201032389641‬‏*`, false, { mentions: [m.sender] });
         await this.updateBlockStatus(m.chat, 'block');
   
         // Return true to indicate that the private message should be blocked
