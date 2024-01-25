9014b2493b5026d4ea798656e6163835095f4b73
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-25 23:11:12 +0200

    Update gp-demote.js

--- plugins/gp-demote.js
+++ plugins/gp-demote.js
@@ -1,35 +1,34 @@
-let handler = async (m, { conn,usedPrefix, command, text }) => {
-if(isNaN(text) && !text.match(/@/g)){
-	
-}else if(isNaN(text)) {
-var number = text.split`@`[1]
-}else if(!isNaN(text)) {
-var number = text
-}
-if(!text && !m.quoted) return conn.reply(m.chat, `✳️ Using the command \n *${usedPrefix + command}* @tag`, m)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `✳️ Number incorrect`, m)
-  
-try {
-if(text) {
-var user = number + '@s.whatsapp.net'
-} else if(m.quoted.sender) {
-var user = m.quoted.sender
-} else if(m.mentionedJid) {
-var user = number + '@s.whatsapp.net'
-} 
-} catch (e) {
-} finally {
-conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-m.reply(`✅ User demoted`)
-}
+const handler = async (m, {conn, usedPrefix, text}) => {
+  if (isNaN(text) && !text.match(/@/g)) {
 
-}
-handler.help = ['demote (@tag)']
-handler.tags = ['group']
-handler.command = ['demote', 'degradar'] 
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-handler.fail = null
+  } else if (isNaN(text)) {
+    var number = text.split`@`[1];
+  } else if (!isNaN(text)) {
+    var number = text;
+  }
 
-export default handler
+  if (!text && !m.quoted) return conn.reply(m.chat, `*[❗] استخدام صحيح*\n\n*┯┷*\n*┠≽ ${usedPrefix}تنزيل الادمن @تاغ*\n*┠≽ ${usedPrefix}تنزيل الادمن -> قم بالرد على الرسالة*\n*┷┯*`, m);
+  if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*[ ⚠️ ] الرقم الذي أدخلته غير صحيح، يرجى إدخال رقم صحيح*`, m);
+
+  try {
+    if (text) {
+      var user = number + '@s.whatsapp.net';
+    } else if (m.quoted.sender) {
+      var user = m.quoted.sender;
+    } else if (m.mentionedJid) {
+      var user = number + '@s.whatsapp.net';
+    }
+  } catch (e) {
+  } finally {
+    conn.groupParticipantsUpdate(m.chat, [user], 'demote');
+    conn.reply(m.chat, `*[ ✅ ] تم تنزيله من الاشراف*`, m);
+  }
+};
+handler.help = ['*2011xxx*', '*@usuario*', '*responder chat*'].map((v) => 'demote ' + v);
+handler.tags = ['group'];
+handler.command = /^(demote|إزالة المشرف|ازالة المشرف|خفض|تخفيض)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+handler.fail = null;
+export default handler;
