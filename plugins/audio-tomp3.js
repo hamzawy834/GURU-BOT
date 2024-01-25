75f812ab876b6dee874eea1dac0154334e6e1119
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-25 19:06:29 +0200

    Update audio-tomp3.js

--- plugins/audio-tomp3.js
+++ plugins/audio-tomp3.js
@@ -5,13 +5,13 @@ let handler = async (m, { conn, usedPrefix, command }) => {
    /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
     if (!/video|audio/.test(mime)) throw `✳️ Reply to the video or voice note you want to convert to mp3 with the command :\n\n*${usedPrefix + command}*`*/
     let media = await q.download?.()
-    if (!media) throw '❎ Failed to download media'
+    if (!media) throw '❎ فشل تحميل الوسائط'
     let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw '❎ Error converting'
+    if (!audio.data) throw '❎ حدث خطأ أثناء التحويل'
     conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
 }
 handler.help = ['tomp3']
 handler.tags = ['fun']
-handler.command = /^to(mp3|a(udio)?)$/i
+handler.command = /^ل(صوت|a(udio)?)$/i
 
 export default handler
