e91a8118fc2a8f3dbd7f6fed4ea59904c50c509f
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-26 00:04:35 +0200

    Update fun-truth.js

--- plugins/fun-truth.js
+++ plugins/fun-truth.js
@@ -1,16 +1,18 @@
 import fetch from 'node-fetch'
+import { translate } from '@vitalets/google-translate-api';
 let handler  = async (m, { conn }) => {
     let shizokeys = 'shizo'	
   let res = await fetch(`https://shizoapi.onrender.com/api/texts/truth?apikey=${shizokeys}`)
   if (!res.ok) throw await res.text()
 	    let json = await res.json()
 
-  let guru = `${json.result}`
-  conn.sendMessage(m.chat, { text: guru, mentions: [m.sender] }, { quoted: m })
-}
+  let translatedText = await translate(json.result, { to: 'ar' });
+
+  conn.sendMessage(m.chat, { text: translatedText.text, mentions: [m.sender] }, { quoted: m });
+};
 handler.help = ['truth']
 handler.tags = ['fun']
-handler.command = /^(truth)$/i
+handler.command = /^(حقيقه)$/i
 
 export default handler
 
