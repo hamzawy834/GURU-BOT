5420ec3692b351db9fa16fe06b5d49bd07c3a0cd
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-25 21:30:37 +0200

    Update dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -3,17 +3,17 @@ import { download } from 'aptoide-scraper';
 let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
   try {
     if (command === 'modapk') {
-      if (!text) throw `*[❗] Please provide the APK Name you want to download.*`;
+      if (!text) throw `*[❗] الرجاء تقديم اسم تطبيق APK الذي تريد تنزيله.*`;
 
       await conn.reply(m.chat, global.wait, m);
       let data = await download(text);
 
       if (data.size.replace(' MB', '') > 200) {
-        return await conn.sendMessage(m.chat, { text: '*[⛔] The file is too large.*' }, { quoted: m });
+        return await conn.sendMessage(m.chat, { text: '*[⛔] الملف كبير جدًا.*' }, { quoted: m });
       }
 
       if (data.size.includes('GB')) {
-        return await conn.sendMessage(m.chat, { text: '*[⛔] The file is too large.*' }, { quoted: m });
+        return await conn.sendMessage(m.chat, { text: '*[⛔] الملف كبير جدًا.*' }, { quoted: m });
       }
 
       await conn.sendMessage(
@@ -23,11 +23,11 @@ let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
       )
     }
   } catch {
-    throw `*[❗] An error occurred. Make sure to provide a valid link.*`;
+    throw `*[❗] حدث خطأ. تأكد من تقديم رابط صالح.*`;
   }
 };
 
 handler.help = ['modapk']
 handler.tags = ['downloader']
-handler.command = /^modapk$/i;
+handler.command = /^تطبيق$/i;
 export default handler;
