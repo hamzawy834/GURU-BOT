e18d083b3fcd7bae220a15f2abf50f010e59443a
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-25 18:30:13 +0200

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -16,8 +16,8 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
         }
         await conn.reply(m.chat, `*≡ Link Detected*
             
-We do not allow links from other groups 
-I'm sorry *@${m.sender.split('@')[0]}*  you will be kicked out of the group ${isBotAdmin ? '' : '\n\nIM not an admin so I canT expel you :"v'}`, null, { mentions: [m.sender] } )
+نحن لا نسمح بالروابط من المجموعات الأخرى
+أنا آسف *@${m.sender.split('@')[0]}* سيتم طردك من المجموعة ${isBotAdmin ? '' : '\n\nأنا لست مسؤولاً لذا لا أستطيع طردك :"v'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
         	await conn.sendMessage(m.chat, { delete: m.key })
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
