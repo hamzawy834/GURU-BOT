7b9f4e81d6f7a2766bc57b44df0caf8223fd2395
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-25 19:12:41 +0200

    Update cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -2,12 +2,12 @@
 
 let handler = async (m, { conn }) => {
     conn.reply(m.chat, `
-*LIST  COMANDOS*
+*قائمة الأوامر*
 
-▢ *Info:* If it is in *bold* it is blocked
+▢ *معلومات:* إذا كانت في *خط عريض* فهي محظورة
 
 ──────────────────
-${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(bloqueado) ${key}` : key} : ${value.text}`).join('\n')}
+${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(محظور) ${key}` : key} : ${value.text}`).join('\n')}
 
 `.trim(), null, {
         mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])
