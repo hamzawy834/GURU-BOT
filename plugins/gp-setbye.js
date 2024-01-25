f77f2a9ab8cf02d1b8cad0a805af46d8f9493de5
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-25 23:36:55 +0200

    Update gp-setbye.js

--- plugins/gp-setbye.js
+++ plugins/gp-setbye.js
@@ -3,12 +3,12 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     global.db.data.chats[m.chat].sBye = text
-    m.reply('✅ The farewell message was established')
-  } else throw `✳️ enter the message\n@user (mención)`
+    m.reply('✅ تم تعيين رسالة الوداع بنجاح')
+  } else throw `✳️ الرجاء إدخال الرسالة\n@شخص (منشن)`
 }
 handler.help = ['setbye <text>']
 handler.tags = ['group']
-handler.command = ['setbye'] 
+handler.command = ['تغيرالمغادره'] 
 handler.admin = true
 handler.owner = false
 
