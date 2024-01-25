00d1d468b14cc827bb1e76db820956ba5dff4fbc
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-25 21:44:25 +0200

    Update econ-addxp.js

--- plugins/econ-addxp.js
+++ plugins/econ-addxp.js
@@ -4,27 +4,27 @@ let handler = async (m, { conn, text }) => {
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw '✳️ Tag the user'
+  if (!who) throw '✳️ قم بمنشنه على المستخدم'
   let txt = text.replace('@' + who.split`@`[0], '').trim()
-  if (!txt) throw '✳️ Enter the amount of *XP* you want to add'
-  if (isNaN(txt)) throw ' 🔢 only numbers'
+  if (!txt) throw '✳️ أدخل كمية *الخبرة* التي تريد إضافتها'
+  if (isNaN(txt)) throw ' 🔢 الرقم فقط'
   let xp = parseInt(txt)
   let exp = xp
   
-  if (exp < 1) throw '✳️ Mínimum *1*'
+  if (exp < 1) throw '✳️ الحد الأدنى *1*'
   let users = global.db.data.users
   users[who].exp += xp
 
-  await m.reply(`≡ *XP ADDED*
+  await m.reply(`≡ *تمت إضافة الخبرة*
 ┌──────────────
-▢  *Total:* ${xp}
+▢  *المجموع:* ${xp}
 └──────────────`)
  conn.fakeReply(m.chat, `▢ Did you recieve \n\n *+${xp} XP*`, who, m.text)
 }
 
 handler.help = ['addxp <@user>']
 handler.tags = ['economy']
-handler.command = ['addxp'] 
+handler.command = ['خبره'] 
 handler.rowner = true
 
 export default handler
