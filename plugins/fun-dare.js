4aa5bc936a4a7dac142e194bf86bad5da5287701
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-26 01:48:40 +0200

    Update fun-dare.js

--- plugins/fun-dare.js
+++ plugins/fun-dare.js
@@ -1,16 +1,33 @@
-import fetch from 'node-fetch'
-let handler  = async (m, { conn }) => {
-    let shizokeys = 'shizo'
-  let res = await fetch(`https://shizoapi.onrender.com/api/texts/dare?apikey=${shizokeys}`)
-  if (!res.ok) throw await res.text()
-	    let json = await res.json()
+import fetch from 'node-fetch';
+import translate from '@vitalets/google-translate-api';
 
-  let guru = `${json.result}`
-  conn.sendMessage(m.chat, { text: guru, mentions: [m.sender] }, { quoted: m })
-}
-handler.help = ['dare']
-handler.tags = ['fun']
-handler.command = /^(dare)$/i
+let yoMamaJokeHandler = async (m, { conn, text }) => {
+  try {
+    let res = await fetch(`https://shizoapi.onrender.com/api/texts/dare?apikey=shizo`);
+
+    if (!res.ok) {
+      throw new Error(`فشل طلب API مع الحالة ${res.status}`);
+    }
+
+    let json = await res.json();
+
+    console.log('JSON response:', json);
+
+    let yoMamaJoke = `${json.result}`;
+    
+    let translation = await translate(yoMamaJoke, { to: 'ar' });
+
+    let translatedYoMamaJoke = translation.text;
+
+    m.reply(translatedYoMamaJoke);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+yoMamaJokeHandler.help = ['yomamajoke'];
+yoMamaJokeHandler.tags = ['fun'];
+yoMamaJokeHandler.command = /^(تجرؤ)$/i;
 
 export default handler
 
