54c7bc6284d47393652fa383a9e19b40b08c275f
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-25 21:26:46 +0200

    Update dl-mediafire.js

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -6,8 +6,8 @@ let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems }) =
 	var limit
      if((isOwner || isPrems)) limit = 1200
      else limit = 100
-   if (!args[0]) throw `✳️ Enter the mediafire link next to the command`
-    if (!args[0].match(/mediafire/gi)) throw `❎ Link incorrect`
+   if (!args[0]) throw `✳️ أدخل رابط ميديافاير بجوار الأمر`
+    if (!args[0].match(/mediafire/gi)) throw `❎ الرابط غير صحيح`
     m.react(rwait)
     let full = /f$/i.test(command)
     let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
@@ -16,13 +16,13 @@ let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems }) =
     let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
     let isLimit = (isPrems || isOwner ? limit : limit) * 1012 < filesize
     let caption = `
-   ≡ *MEDIAFIRE*
+   ≡ *ميديافاير*
 
-▢ *Number:* ${filename}
-▢ *Size:* ${filesizeH}
-▢ *Extension:* ${ext}
-▢ *Uploaded:* ${aploud}
-${isLimit ? `\n▢ The file exceeds the download limit *+${limit} MB*\nUpgrade to premium to be able to download files more than *900 MB*` : ''} 
+▢ *الرقم:* ${filename}
+▢ *الحجم:* ${filesizeH}
+▢ *الامتداد:* ${ext}
+▢ *تم التحميل:* ${aploud}
+${isLimit ? `\n▢ الملف يتجاوز حد التنزيل *+${limit} ميغابايت*\nقم بالترقية إلى حساب بريميوم لتكون قادرًا على تنزيل ملفات أكبر من *900 ميغابايت*` : ''} 
 `.trim()
     await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)
     
@@ -31,7 +31,7 @@ ${isLimit ? `\n▢ The file exceeds the download limit *+${limit} MB*\nUpgrade t
 }
 handler.help = ['mediafire <url>']
 handler.tags = ['downloader', 'premium']
-handler.command = ['mediafire', 'mfire'] 
+handler.command = ['mediafire', 'ميديا-فاير'] 
 handler.credit = true
 handler.premium = false
 
