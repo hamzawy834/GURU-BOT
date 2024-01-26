0e9f71771d048b87d42d100be945a63f347a15f8
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-26 04:31:48 +0200

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
 	
 
-  let isEnable = /true|enable|(turn)?تشغيل|1/i.test(command)
+  let isEnable = /true|تشغيل|(turn)?تشغيل|1/i.test(command)
   let chat = global.db.data.chats[m.chat]
   let user = global.db.data.users[m.sender]
   let bot = global.db.data.settings[conn.user.jid] || {}
